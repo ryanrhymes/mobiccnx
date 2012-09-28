d712b8b4964b8414d9a6636bb83890fe821574fc
Liang Wang <ryanrhymes@gmail.com>
2012-09-28 22:24:16 +0300

    add deploy script

--- tool/deploy_ccnd_conf.sh
+++ tool/deploy_ccnd_conf.sh
@@ -16,5 +16,11 @@ echo "generating ccnd.conf for nodes ..."
 $pyapp $topology
 
 echo "deploying ccnd.conf on nodes ..."
+arr=`cat $topology | sed '/^$/d' | grep -v "^#" | sed 's/ -> /\n/' | sort -n | uniq`
+
+for x in $arr; do
+    ssh -o BatchMode=yes -o StrictHostKeyChecking=no $node "if [ ! -d /var/tmp/ccnd ]; then mkdir /var/tmp/ccnd; fi; chgrp cone /var/tmp/ccnd; chmod 0770 /var/tmp/ccnd; exit;" &
+    scp ./tmp_confs/ccnd.conf.$x $x:/var/tmp/ccnd/ccnd.conf.$x
+done
 
 echo "Done!"
