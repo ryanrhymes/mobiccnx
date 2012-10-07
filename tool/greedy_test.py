b06d6acf77c1894a1fbe1c92b8f46af1d9f4dfb3
Liang Wang <ryanrhymes@gmail.com>
2012-10-07 22:00:33 +0300

    Add test module for GreedyEmbedding

---
+++ tool/greedy_test.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# 
+# This script tests the functions in GreedyEmbedding. It also shows
+# how to calculate the distance of any two points in Poincare disk.
+#
+# Reference: 
+# R. Kleinberg - Geographic routing using hyperbolic space
+# A. Cvetkovski - Hyperbolic Embedding and Routing for Dynamic Graphs
+#
+# Liang Wang @ Dept. of Computer Science, Univ. of Helsinki, Finland
+# 2012.10.07 created
+#
+
+import re
+import os
+import sys
+import cmath
+
+from greedy_embedding import GreedyEmbedding
+
+
+if __name__=="__main__":
+    greed = GreedyEmbedding()
+    sys.exit(0)
